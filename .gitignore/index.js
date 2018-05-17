2b3ca5243137745cefea4e29b316fef43f3c3569
REGDYLAN <39389021+REGDYLAN@users.noreply.github.com>
2018-05-17 23:57:43 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,61 @@
+/* Inclure - BOT REGDYLAN */
+
+const discord = require("discord.js");
+
+/* Variables - BOT REGDYLAN */
+
+const clientDiscord = new discord.Client();
+const PREFIX = "@";
+
+/* Evenement - BOT REGDYLAN */
+
+clientDiscord.on("ready", () => {
+    console.log("Prêt à travailler!");
+    //clientDiscord.channels.get("415567638530490378").send("Bot allumé!");
+});
+
+clientDiscord.on("message", message => {
+    if(message.content[0] === PREFIX) {
+        let spliteMessage = message.content.split(' '); 
+        if(spliteMessage[0] === "@hello") {
+            //message.reply("Bonjour! Comment tu vas?");
+            message.channel.send("Bonjour! Comment tu vas?");
+            //clientDiscord.channels.get("415567638530490378").send("Commande exécuté par: " + message.author.username);
+        }
+        else if(spliteMessage[0] === "@commande") {
+            if(spliteMessage.length === 2) {
+                message.channel.send("Le paramètre: " + spliteMessage[1]);
+            }
+            else
+                message.reply("Problème dans les paramètres");
+        }
+        else if(spliteMessage[0] === "@markdown") {
+            message.channel.send(
+`**Coucou**, c'est *moi*! ***Le bot REGDYLAN***
+
+~~Coucou!~~
+ __kikou__
+`);
+            message.channel.send("`Code`");
+        }
+
+        else if(message.content === "@rich1") {
+            message.channel.send({embed: {
+                color: 3447003,
+                description: 'Salut moi c est le Bot!'
+            }});
+
+        }
+    }
+});
+
+clientDiscord.on("guildMemberAdd", member => {
+    member.createDM().then(channel => {
+        return channel.send("Bienvenue sur DestinyLife " + member.displayName);
+    }).catch(console.error);
+
+})
+
+/* Connexion - BOT REGDYLAN */
+
+clientDiscord.login("NDQ2NzU3NzM1NjIzNjg4MTkz.Dd91uQ.W2vFXtDMjiil8hC-Tg5YBT1i65A");
